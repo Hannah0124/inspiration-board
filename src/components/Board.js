65c17927f927e6903ece16662c2e14a375b371fb
Catherina <catherine.pchelina@gmail.com>
2020-04-23 16:34:41 -0700

    Add Wave 2 implementation: add useEffect with axios

--- src/components/Board.js
+++ src/components/Board.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { Component, useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import axios from 'axios';
 
@@ -7,10 +7,36 @@ import Card from './Card';
 import NewCardForm from './NewCardForm';
 import CARD_DATA from '../data/card-data.json';
 
-const Board = () => {
+const Board = (props) => {
+  const BASE_URL = `${props.url}${props.boardName}/cards`
 
-  const cardComponents = CARD_DATA.cards.map((card, i) => {
+  const[cards, setCards] = useState([]);
+  const[errorMessage, setErrorMessage] = useState(null);
 
+  useEffect(() => {
+    axios.get(BASE_URL)
+    .then((response) => {
+     const apiData = response.data;
+     console.log(apiData);
+
+     const cardObjects = apiData.map((cardWrapper, i) => {
+      return {
+        id: cardWrapper.card.id,
+        text: cardWrapper.card.text,
+        emoji: cardWrapper.card.emoji
+      }
+     });
+     setCards(cardObjects);
+    })
+    .catch((error) => {
+      setErrorMessage(error.message);
+    });
+
+  }, []);
+
+  
+  
+  const cardComponents = cards.map((card, i) => {
     return (
       <Card 
       id={card.id}
@@ -21,16 +47,18 @@ const Board = () => {
     );
   });
 
-
-
   return (
     <div>
+      {errorMessage && <div><h2>{errorMessage}</h2></div>}
       {cardComponents}
     </div>
   )
 };
-Board.propTypes = {
 
+
+Board.propTypes = {
+  url: PropTypes.string.isRequired,
+  boardName: PropTypes.string.isRequired
 };
 
 export default Board;
